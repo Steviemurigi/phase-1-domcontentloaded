b9047cd1a4cc80108af1f96b02804d8711ae5293
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-07-24 06:53:43 -0500

    Final change

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+const paragraphInput = document.getElementById("text");
+paragraphInput.textContent = 'This is really cool!'
+})
